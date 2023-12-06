02a51ccca51e4467b705d6fa69997d6d1555c5f8
= <=paulobeltramin.27@gmail.com>
2023-12-06 19:14:13 -0300

    alterado formato do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,146 +1,39 @@
 <script setup>
+  
 
 </script>
 
 <template>
-    <div class="container">
-        <header class="body ">
-            <div>
-                <h1 class="fw-bold text-center mt-4 p-3 text-white">Calculadora</h1>
-            </div>
-        </header>
+    <main class="container bg-secondary p-5">
+        <h1 class="text-center mt-4 text-white mb-4">Calculadora</h1>
 
-        <section>
-            <div class="container_body">
-                <div class="d-flex justify-content-center  align-center p-4 bg-black w-100 ">
-                    <span class="text-white fw-bold fs-2">124,00</span>
+        <div class="row ">
+            <form class="d-flex  flex-column align-items-center">
+
+                <div class=" w-50 mb-4 bg-black text-center text-white p-3 fs-4">
+                    1250
                 </div>
 
-                <div class="row mt-4">
-                    <div class="col d-flex fw-bold fs-2 text-center fs-3 ">
-                        <p class="col-3  border border-secundary pointer p-1 ">
-                            C
-                        </p>
+                <div class="col">
+                    <input class="form-control w-100 mb-4" type="number" placeholder="digite o valor 1">
+                    <input class="form-control w-100 " type="number" placeholder="digite o valor 2">
 
-                        <p class="col-3  border border-secundary pointer p-1">
-                            ()
-                        </p>
+                    <select class="bg-black text-white fw-bold p-1 mt-4">
+                        <option value="somar">Somar</option>
+                        <option value="subtrair">Subtrair</option>
+                        <option value="multiplicar">Multiplicar</option>
+                        <option value="dividir">Dividir</option>
+                    </select>
 
-                        <p class="col-3  border border-secundary pointer p-1">
-                            %
-                        </p>
-
-                        <p class="col-3 pointer">
-                            ÷
-                        </p>
-                    </div>
                 </div>
 
-                <div class="row">
-                    <div class="col d-flex fw-bold p-3 text-center fs-3 ">
-                        <p class="col-3  border border-secundary pointer p-1">
-                            4
-                        </p>
+            </form>
 
-                        <p class="col-3  border border-secundary pointer p-1">
-                            5
-                        </p>
-
-                        <p class="col-3  border border-secundary pointer p-1">
-                            6
-                        </p>
-
-                        <p class="col-3 pointer">
-                            x
-                        </p>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col d-flex fw-bold p-3 text-center fs-3 ">
-                        <p class="col-3  border border-secundary pointer p-1">
-                            4
-                        </p>
-
-                        <p class="col-3  border border-secundary pointer p-1">
-                            5
-                        </p>
-
-                        <p class="col-3  border border-secundary pointer p-1">
-                            6
-                        </p>
-
-                        <p class="col-3 pointer fs-1">
-                            -
-                        </p>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col d-flex fw-bold p-3 text-center fs-3 ">
-                        <p class="col-3  border border-secundary pointer p-1">
-                            1
-                        </p>
-
-                        <p class="col-3  border border-secundary pointer p-1">
-                            2
-                        </p>
-
-                        <p class="col-3  border border-secundary pointer p-1">
-                            3
-                        </p>
-
-                        <p class="col-3 pointer">
-                            +
-                        </p>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col d-flex fw-bold p-3 text-center fs-3 ">
-                        <p class="col-3  border border-secundary pointer p-1">
-                            +/-
-                        </p>
-
-                        <p class="col-3   border border-secundary pointer p-1">
-                            0
-                        </p>
-
-                        <p class="col-3  border border-secundary pointer p-1">
-                            ,
-                        </p>
-
-                        <p class="col-3 pointer ">
-                            =
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
+        </div>
+    </main>
 </template>
   
-<style scoped>
-.body {
-    background-color: grey;
-}
-
-.container_body {
-    background-color: rgb(223, 196, 162);
-    height: 600px;
-    width: 400px;
-    margin: 0 auto;
-}
-
-.pointer {
-    cursor: pointer;
-
-}
-
-.pointer:hover {
-    background-color: antiquewhite;
-}
-</style>
+<style scoped></style>
 
 
 
