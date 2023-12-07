9a388d5b619a1d58f4e69f93a4c87a5bae78b8ce
= <=paulobeltramin.27@gmail.com>
2023-12-06 23:16:11 -0300

    finalizado construção da calculadora em vueJS

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,66 @@
 <script setup>
 import { reactive } from 'vue';
+import Forms from "./components/Forms.vue"
 
+const state = reactive({
 
-const estado = reactive({
     value2: "",
+
     value1: "",
-    result: "",
+
     calculation: "somar",
 
+    result: "0",
 
 })
 
 // function filter
-const setFilter = (event) => {
-    return estado.calculation = event.target.value
+const filterSelect = (event) => {
+    return state.calculation = event.target.value
 }
 
 
 //    input 1
-const valueOne = (event) => {
-    return estado.value1 = estado.result = event.target.value
+const oneValue = (event) => {
+    return state.result = state.value1 = event.target.value
+
 
 }
 //  input2
-const valueTwo = (event) => {
-    return estado.value2 = estado.result = event.target.value
+const twoValue = (event) => {
+    return state.result = state.value2 = event.target.value;
+
 
 }
 
 // function calculation
-const getCalcFilter = () => {
-    const filtro = estado.calculation;
+const setCalcFilter = () => {
+    const filtro = state.calculation;
 
     switch (filtro) {
         case "somar":
-            return estado.value1 + estado.value2;
+            return Number(state.value1) + Number(state.value2);
 
         case "subtrair":
-            return estado.value1 - estado.value2;
+            return state.value1 - state.value2;
 
         case "multiplicar":
-            return estado.value1 * estado.value2;
+            return state.value1 * state.value2;
 
         case "dividir":
-
-            return estado.value1 / estado.value2
+            return state.value1 / state.value2;
 
         default:
 
     }
-
 
 }
 
 </script>
 
 <template>
-    <main class="container bg-secondary p-5">
-        <h1 class="text-center mt-4 text-white mb-4">Calculadora</h1>
-
-        <div class="row ">
-            <form class="d-flex  flex-column align-items-center">
-
-                <div class=" w-50 mb-4 bg-black text-center text-white p-3 fs-4">
-                    {{ getCalcFilter() }}
-                </div>
-
-                <div class="col">
-                    <input class="form-control w-100 mb-4" type="number" placeholder="digite o valor 1" @keyup="valueOne">
-                    <input class="form-control w-100 " type="number" placeholder="digite o valor 2" @keyup="valueTwo">
-
-                    <select class="bg-black text-white fw-bold p-1 mt-4" @change="setFilter">
-                        <option value="somar">Somar</option>
-                        <option value="subtrair">Subtrair</option>
-                        <option value="multiplicar">Multiplicar</option>
-                        <option value="dividir">Dividir</option>
-                    </select>
-
-                </div>
-
-            </form>
-
-        </div>
-    </main>
+    <Forms :filter="setCalcFilter()" :one-value="oneValue" :two-value="twoValue" :value1="state.value1"
+        :value2="state.value2" :filter-select="filterSelect" />
 </template>
   
 <style scoped></style>
---
+++ src/components/Forms.vue
@@ -0,0 +1,48 @@
+
+<script setup>
+
+const props = defineProps([
+
+    "value1",
+    "value2",
+    "filter",
+    "oneValue",
+    "twoValue",
+    "filterSelect"
+])
+
+
+</script>
+
+
+
+<template>
+    <main class="container bg-secondary p-5 rounded-5 mt-5 ">
+        <h1 class="text-center mt-4 text-white mb-4">Calculadora</h1>
+
+        <div class="row ">
+            <form class="d-flex  flex-column align-items-center">
+
+                <input type="url" class=" w-50 mb-4 bg-black text-center text-white p-3 fs-4 rounded-4"
+                    :value="props.filter" />
+
+                <div class="col">
+                    <input :value="props.value1" class="form-control w-100 mb-4" type="number"
+                        placeholder="digite o valor 1" @keyup="props.oneValue">
+                    <input :value="props.value2" class="form-control w-100 " type="number" placeholder="digite o valor 2"
+                        @keyup="props.twoValue">
+
+                    <select class="bg-black text-white fw-bold p-1 mt-4" @change="props.filterSelect">
+                        <option value="somar">Somar</option>
+                        <option value="subtrair">Subtrair</option>
+                        <option value="multiplicar">Multiplicar</option>
+                        <option value="dividir">Dividir</option>
+                    </select>
+
+                </div>
+
+            </form>
+
+        </div>
+    </main>
+</template>
